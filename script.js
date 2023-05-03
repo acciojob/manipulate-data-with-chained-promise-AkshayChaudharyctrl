c2566b6528e94386386824399986709cd75e4615
AkshayChaudharyctrl <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-05-03 12:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ const table = document.querySelector('table');
+ const lastRow = document.createElement('tr');
+ const prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ const child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+
+
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
